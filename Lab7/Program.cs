a28cb433635c33103d92c3ea98964148f0188096
Vladimir Klimov <vladimirklimoff@yandex.ru>
2019-02-04 12:07:52 +0500

    update

--- Lab7/Program.cs
+++ Lab7/Program.cs
@@ -33,6 +33,15 @@ namespace Lab7
             Console.WriteLine("4. Удалить список");
             Console.WriteLine("5. Назад");
         }
+
+        public static void PrintMenu2ndLevelTree()
+        {
+            Console.WriteLine("1. Сформировать идеально сбалансированное дерево");
+            Console.WriteLine("2. Распечатать дерево");
+            Console.WriteLine("3. Найти минимальный элемент в дереве");
+            Console.WriteLine("4. Преобразовать идеально сбалансированное дерево в дерево поиска");
+            Console.WriteLine("5. Назад");
+        }
         /// <summary>
         /// Ввод числа
         /// </summary>
@@ -68,6 +77,9 @@ namespace Lab7
 
     class Program
     {
+        /// <summary>
+        /// Элемент однонаправленного списка
+        /// </summary>
         class Point
         {
             public int data;//информационное поле
@@ -89,13 +101,79 @@ namespace Lab7
         }
 
         /// <summary>
-        /// Создание элемента списка
+        /// Элемент двунаправленного списка
+        /// </summary>
+        class Point2
+        {
+            public int data;//информационное поле
+            public Point2 next, pred;//адресное поле
+            public Point2()//конструктор без параметров
+            {
+                data = 0;
+                next = null;
+                pred = null;
+            }
+            public Point2(int d)//конструктор с параметрами
+            {
+                data = d;
+                next = null;
+                pred = null;
+            }
+            public override string ToString()
+            {
+                return data + " ";
+            }
+        }
+
+        /// <summary>
+        /// Элемент бинарного дерева
+        /// </summary>
+        class PointTree
+        {
+            public int data;
+            public PointTree left,//адрес левого поддерева 
+                         right;//адрес правого поддерева
+            public PointTree()
+            {
+                data = 0;
+                left = null;
+                right = null;
+            }
+
+            public PointTree(int d)
+            {
+                data = d;
+                left = null;
+                right = null;
+            }
+
+            public override string ToString()
+            {
+                return data + " ";
+            }
+
+
+        }
+
+        /// <summary>
+        /// Создание элемента однонаправленного списка
         /// </summary>
         /// <param name="d">Значение информационного поля</param>
-        /// <returns>Элемент списка</returns>
+        /// <returns>Элемент однонаправленного списка</returns>
         static Point MakePoint(int d)
         {
             Point p = new Point(d);
+            return p;
+        }
+
+        /// <summary>
+        /// Создание элемента двунаправленного списка
+        /// </summary>
+        /// <param name="d">Значение информационного поля</param>
+        /// <returns>Элемент двунаправленного списка</returns>
+        static Point2 MakePoint2(int d)
+        {
+            Point2 p = new Point2(d);
             return p;
         }
 
@@ -117,6 +195,30 @@ namespace Lab7
                 //создаем элемент и добавляем в начало списка
                 Point p = MakePoint(info);
                 p.next = beg;
+                beg = p;
+            }
+            return beg;
+        }
+
+        /// <summary>
+        /// Добавление в начало двунаправленного списка
+        /// </summary>
+        /// <param name="size">>Количество элементов списка</param>
+        /// <returns>Ссылка на начальный элемент списка</returns>
+        static Point2 MakeDoublyList(int size)
+        {
+            Random rnd = new Random();
+            int info = rnd.Next(0, 11);
+            Console.WriteLine("Элемент {0} добавляется...", info);
+            Point2 beg = MakePoint2(info);//создаем первый элемент
+            for (int i = 1; i < size; i++)
+            {
+                info = rnd.Next(0, 11);
+                Console.WriteLine("Элемент {0} добавляется...", info);
+                //создаем элемент и добавляем в начало списка
+                Point2 p = MakePoint2(info);
+                p.next = beg;
+                beg.pred = p;
                 beg = p;
             }
             return beg;
@@ -147,7 +249,7 @@ namespace Lab7
         }
 
         /// <summary>
-        /// Отображение списка
+        /// Отображение однонаправленного списка
         /// </summary>
         /// <param name="beg">Ссылка на начальный элемент</param>
         static void ShowList(Point beg)
@@ -168,7 +270,28 @@ namespace Lab7
         }
 
         /// <summary>
-        /// Удаление элемента из списка
+        /// Отображение двунаправленного списка
+        /// </summary>
+        /// <param name="beg">Ссылка на начальный элемент</param>
+        static void ShowList(Point2 beg)
+        {
+            //проверка наличия элементов в списке
+            if (beg == null)
+            {
+                Console.WriteLine("Список пуст");
+                return;
+            }
+            Point2 p = beg;
+            while (p != null)
+            {
+                Console.Write(p);
+                p = p.next;//переход к следующему элементу
+            }
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Удаление элемента из однонаправленного списка
         /// </summary>
         /// <param name="beg">Ссылка на первый элемент</param>
         /// <param name="number">Номер элемента</param>
@@ -195,6 +318,39 @@ namespace Lab7
             }
             //исключаем элемент из списка
             p.next = p.next.next;
+            return beg;
+        }
+
+        /// <summary>
+        /// Удаление элемента из двунаправленного списка
+        /// </summary>
+        /// <param name="beg">Ссылка на первый элемент</param>
+        /// <param name="number">Номер элемента</param>
+        /// <returns>Ссылка на первый элемент или null, если элементов нет</returns>ram>
+        /// <returns></returns>
+        static Point2 DelElement(Point2 beg, int number)
+        {
+            if (beg == null)//пустой список
+            {
+                return null;
+            }
+            if (number == 1)//удаляем первый элемент
+            {
+                beg = beg.next;
+                return beg;
+            }
+            Point2 p = beg;
+            //ищем элемент для удаления и встаем на предыдущий
+            for (int i = 1; i < number - 1 && p != null; i++)
+                p = p.next;
+            if (p == null)//если элемент не найден
+            {
+                Console.WriteLine("Весь список удален");
+                return beg;
+            }
+            //исключаем элемент из списка
+            p.next = p.next.next;
+            p.next.pred = null;
             return beg;
         }
 
@@ -227,6 +383,103 @@ namespace Lab7
         }
 
         /// <summary>
+        /// Добавление элемента с заданным номером в двунаправленный список
+        /// </summary>
+        /// <param name="beg">Ссылка на начало списка</param>
+        /// <param name="number">Номер элемента списка</param>
+        /// <returns>Ссылка на первый элемент</returns>
+        static Point2 AddPoint(Point2 beg, int number)
+        {
+            Random rnd = new Random();
+            int info = rnd.Next(10, 100);
+            Console.WriteLine("Элемент {0} добавляется...", info);
+            Point2 NewPoint = MakePoint2(info); //создание нового элемента
+            if (beg == null)//список пустой
+            {
+                beg = MakePoint2(rnd.Next(10, 100));
+                return beg;
+            }
+            if (number == 1) //добавление в начало списка
+            {
+                NewPoint.next = beg;
+                beg.pred = NewPoint;
+                beg = NewPoint;
+                return beg;
+            }
+            //вспом. переменная для прохода по списку
+            Point2 p = beg;
+            //идем по списку до нужного элемента
+            for (int i = 1; i < number - 1 && p != null; i++)
+                p = p.next;
+            if (p == null)//элемент не найден
+            {
+                Console.WriteLine("Ошибка! Размер списка меньше номера элемента");
+                return beg;
+            }
+            //добавляем новый элемент
+            NewPoint.next = p.next;
+            NewPoint.pred = p;
+            p.next = NewPoint;
+            Console.WriteLine("Элемент добавлен");
+            return beg;
+
+        }
+
+        /// <summary>
+        /// Количество элементов в двунаправленном списке
+        /// </summary>
+        /// <param name="beg">Ссылка на первый элемент</param>
+        /// <returns>Количество элементов</returns>
+        static int FindNumberOfElements(Point2 beg)
+        {
+            int numberOfElements = 0;
+            if (beg == null)//пустой список
+            {
+                return 0;
+            }
+            Point2 p = beg;
+            for (int i = 1; p != null; i++)
+            {
+                p = p.next;
+                numberOfElements++;
+            }
+            return numberOfElements;
+                
+        }
+
+
+        static void ShowTree (PointTree p, int l)
+        {
+            if (p != null)
+            {
+                ShowTree(p.left, l + 3);//переход к левому поддереву
+                                        //формирование отступа
+                for (int i = 0; i < l; i++) Console.Write(" ");
+                Console.WriteLine(p.data);//печать узла
+                ShowTree(p.right, l + 3);//переход к правому поддереву
+            }
+
+        }
+
+        static PointTree IdealTree(int size, PointTree p)
+        {
+            PointTree r;
+            int nl, nr;
+            if (size == 0)
+            {
+                p = null;
+                return p;
+            }
+            nl = size / 2;
+            nr = size - nl - 1;
+            int info = Dialog.InputNumber("Введите число от 1 до 10",1,10);
+            r = new PointTree(info);
+            r.left = IdealTree(nl, r.left);
+            r.right = IdealTree(nr, r.right);
+            return r;
+        }
+
+        /// <summary>
         /// Главное меню
         /// </summary>
         /// <param name="exit">Выход из меню</param>
@@ -239,14 +492,14 @@ namespace Lab7
                 switch (menuItemLevel1)
                 {
 
-                    case 1: // Однонаправленый список
+                    case 1: //Однонаправленый список
                         LinkedListSinglyMenu();
                         break;
                     case 2://Двунаправленный список
                         LinkedListDoublyMenu();
                         break;
-                    case 3:
-
+                    case 3://Бинарное дерево
+                        TreeMenu();
                         break;
                     case 4:
                         exit = true;
@@ -317,7 +570,7 @@ namespace Lab7
         private static void LinkedListDoublyMenu()
         {
             int userAnswer;
-            Point startOfList = null;
+            Point2 startOfList = null;
             do
             {
                 Dialog.PrintMenu2ndLevelDoublyLinkedList();
@@ -326,22 +579,36 @@ namespace Lab7
                 {
                     case 1: //сформировать двунаправленный список
                         {
-
+                            startOfList = MakeDoublyList(10);
+                            Console.WriteLine("Список сформирован!");
                             break;
                         }
                     case 2: //распечатать двунаправленный список
                         {
-                            
+                            ShowList(startOfList);
                             break;
                         }
                     case 3: //добавить в список элемент с заданным номером
                         {
-     
+                            int number = Dialog.InputNumber("Введите номер элемента списка");
+                            startOfList = AddPoint(startOfList, number);     
                             break;
                         }
                     case 4: //удалить весь список
                         {
-
+                            int num = FindNumberOfElements(startOfList);
+                            if (num == 0)
+                            {
+                                Console.WriteLine("Список пуст");
+                            }
+                            else
+                            {
+                                for (int i = 1; i <= num; i++)
+                                {
+                                    startOfList = DelElement(startOfList, 1);
+                                }
+                                Console.WriteLine("Список удален");
+                            }
                             break;
                         }
                     default:
@@ -349,7 +616,44 @@ namespace Lab7
                 }
             } while (userAnswer != 5);
         }
-    
+
+        /// <summary>
+        /// Подменю "Бинарное дерево"
+        /// </summary>
+        private static void TreeMenu()
+        {
+            int userAnswer;
+            PointTree startOfTree = null;
+            do
+            {
+                Dialog.PrintMenu2ndLevelTree();
+                userAnswer = Dialog.InputNumber("Введите пункт меню", 1, 5);
+                switch (userAnswer)
+                {
+                    case 1://сформировать идеально сбалансированное дерево
+                        {
+                            startOfTree = IdealTree(7, startOfTree);
+                            break;
+                        }
+                    case 2://распечатать дерево
+                        {
+                            ShowTree(startOfTree, 1);
+                            break;
+                        }
+                    case 3://найти минимальный элемент в дереве
+                        {
+                            break;
+                        }
+                    case 4://преобразовать идеально сбалансированное дерево в дерево поиска
+                        {
+                            break;
+                        }
+                    default:
+                        break;
+                }
+
+            } while (userAnswer != 5);
+        }
 
         static void Main(string[] args)
         {
